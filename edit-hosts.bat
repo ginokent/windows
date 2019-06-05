2a29805fda909fc6aa15c18763c9a78801a576a6
djeeno <djeeeno@gmail.com>
2019-06-05 10:52:58 +0900

    Create edit-hosts.bat

---
+++ edit-hosts.bat
@@ -0,0 +1,17 @@
+@echo off
+rem # 
+rem # - Name
+rem #     edit-hosts.bat
+rem # 
+rem # - Contents
+rem #     Edit "C:\Windows\System32\drivers\etc\hosts"
+rem # 
+rem # - Install
+rem #     powershell -Command "Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/djeeno/windows/master/edit-hosts.bat' -OutFile "$env:USERPROFILE\edit-hosts.bat""
+rem # 
+rem # - Revision
+rem #     2017-05-10 created.
+rem #     yyyy-MM-dd modified.
+rem # 
+
+start powershell -Command "Start-Process -Verb RUNAS notepad C:\Windows\System32\drivers\etc\hosts"
