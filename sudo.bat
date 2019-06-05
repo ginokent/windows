a83686d0fe25b53c5c7ac6397c0438bf14694f08
djeeno <djeeeno@gmail.com>
2019-06-05 09:44:49 +0900

    Create sudo.bat

---
+++ sudo.bat
@@ -0,0 +1,18 @@
+@echo off
+rem # 
+rem # - Name
+rem #     sudo.bat
+rem # 
+rem # - Contents
+rem #     C:\Windows\System32\runas.exe rundll32.exe shell32.dll,#61
+rem #     It means: <kbd>Windows</kbd> + <kbd>R</kbd>
+rem # 
+rem # - Install
+rem #     powershell.exe -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/sudo.bat" -OutFile "$env:USERPROFILE\sudo.bat""
+rem #
+rem # - Revision
+rem #     2017-05-10 created.
+rem #     yyyy-MM-dd modified.
+rem # 
+
+start powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process -Verb RUNAS rundll32.exe shell32.dll,`#61"
