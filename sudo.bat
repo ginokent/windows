c3d5732df95b60c002e2e408e7b05e54873b5832
djeeno <djeeeno@gmail.com>
2019-06-05 10:36:44 +0900

    Update sudo.bat

--- sudo.bat
+++ sudo.bat
@@ -8,11 +8,11 @@ rem #     C:\Windows\System32\runas.exe rundll32.exe shell32.dll,#61
 rem #     It means: <kbd>Windows</kbd> + <kbd>R</kbd>
 rem # 
 rem # - Install
-rem #     powershell.exe -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/sudo.bat" -OutFile "$env:USERPROFILE\sudo.bat""
+rem #     powershell -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/sudo.bat" -OutFile "$env:USERPROFILE\sudo.bat""
 rem #
 rem # - Revision
 rem #     2017-05-10 created.
 rem #     yyyy-MM-dd modified.
 rem # 
 
-start powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process -Verb RUNAS rundll32.exe shell32.dll,`#61"
+start powershell -Command "Start-Process -Verb RUNAS rundll32.exe shell32.dll,`#61"
