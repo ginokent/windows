aeeec1569285e5e51d64d66f81e7f1107fd8faa5
djeeno <djeeeno@gmail.com>
2019-06-05 10:16:40 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,21 @@
+@echo off
+rem # 
+rem # - Name
+rem #     sudo.bat
+rem # 
+rem # - Contents
+rem #     Setup tools
+rem # 
+rem # - Install
+rem #     powershell.exe -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/setup.bat" -OutFile "$env:USERPROFILE\setup.bat""
+rem #
+rem # - Revision
+rem #     2019-06-05 created.
+rem #     yyyy-MM-dd modified.
+rem # 
+
+powershell.exe -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/capslock2ctrl.bat" -OutFile "$env:USERPROFILE\capslock2ctrl.bat""
+
+powershell.exe -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/sudo.bat" -OutFile "$env:USERPROFILE\sudo.bat""
+
 explorer https://github.com/djeeno/windows
