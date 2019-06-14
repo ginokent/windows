2588f59be1366737030c5ed59c1644b9ebeb2abd
djeeno <djeeeno@gmail.com>
2019-06-15 04:28:45 +0900

    Update install-wsl.bat

--- install-wsl.bat
+++ install-wsl.bat
@@ -7,11 +7,11 @@ rem # - Contents
 rem #     Install Windows Subsystem for Linux
 rem # 
 rem # - Install
-rem #     powershell -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/install-wsl.bat" -OutFile "$env:USERPROFILE\install-wsl.bat""
+rem #     powershell "Invoke-WebRequest -Uri https://raw.githubusercontent.com/djeeno/windows/master/install-wsl.bat -OutFile $env:USERPROFILE\install-wsl.bat"
 rem #
 rem # - Revision
 rem #     2019-06-05 created.
 rem #     yyyy-MM-dd modified.
 rem # 
 
-powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux"
+powershell "Start-Process -Verb RUNAS powershell 'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux'"
