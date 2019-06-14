1f5ff3449a63c8bf946d3a6b6d6ac41781e538b5
djeeno <djeeeno@gmail.com>
2019-06-15 04:59:27 +0900

    Update sh.bat

--- sh.bat
+++ sh.bat
@@ -7,7 +7,7 @@ rem # - Contents
 rem #     Open shell
 rem # 
 rem # - Install
-rem #     powershell -Command "Invoke-WebRequest -Uri "https://raw.githubusercontent.com/djeeno/windows/master/sh.bat" -OutFile "$env:USERPROFILE\sh.bat""
+rem #     powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/djeeno/windows/master/sh.bat -OutFile $env:USERPROFILE\sh.bat"
 rem #
 rem # - Revision
 rem #     2019-06-05 created.
