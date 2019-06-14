429a1d772bb5ee362442945418749c8127e3a08b
djeeno <djeeeno@gmail.com>
2019-06-15 04:58:32 +0900

    Update edit-hosts.bat

--- edit-hosts.bat
+++ edit-hosts.bat
@@ -7,7 +7,7 @@ rem # - Contents
 rem #     Edit "C:\Windows\System32\drivers\etc\hosts"
 rem # 
 rem # - Install
-rem #     powershell -Command "Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/djeeno/windows/master/edit-hosts.bat' -OutFile "$env:USERPROFILE\edit-hosts.bat""
+rem #     powershell -Command "Invoke-RestMethod -Uri https://raw.githubusercontent.com/djeeno/windows/master/edit-hosts.bat -OutFile $env:USERPROFILE\edit-hosts.bat"
 rem # 
 rem # - Revision
 rem #     2017-05-10 created.
